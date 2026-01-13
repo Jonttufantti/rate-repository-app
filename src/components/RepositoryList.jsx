5833ebb9c2f966363128fc1aadc92e208c7f1229
Jonttufantti <81356143+Jonttufantti@users.noreply.github.com>
2026-01-13 15:37:18 +0200

    Exercise 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,6 +2,8 @@ import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useNavigate } from "react-router-native";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 
 const styles = StyleSheet.create({
   separator: {
@@ -9,11 +11,28 @@ const styles = StyleSheet.create({
   },
 });
 
+const ORDERINGS = {
+  LATEST: {
+    orderBy: "CREATED_AT",
+    orderDirection: "DESC",
+  },
+  HIGHEST: {
+    orderBy: "RATING_AVERAGE",
+    orderDirection: "DESC",
+  },
+  LOWEST: {
+    orderBy: "RATING_AVERAGE",
+    orderDirection: "ASC",
+  },
+};
+
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
   const navigate = useNavigate();
-  const { repositories, loading, refetch } = useRepositories();
+  const [order, setOrder] = useState("LATEST");
+
+  const { repositories, loading, refetch } = useRepositories(ORDERINGS[order]);
 
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
@@ -23,6 +42,16 @@ const RepositoryList = () => {
     <FlatList
       data={repositoryNodes}
       keyExtractor={(item) => item.id}
+      ListHeaderComponent={
+        <Picker
+          selectedValue={order}
+          onValueChange={(value) => setOrder(value)}
+        >
+          <Picker.Item label="Latest repositories" value="LATEST" />
+          <Picker.Item label="Highest rated repositories" value="HIGHEST" />
+          <Picker.Item label="Lowest rated repositories" value="LOWEST" />
+        </Picker>
+      }
       renderItem={({ item }) => (
         <Pressable onPress={() => navigate(`/repositories/${item.id}`)}>
           <RepositoryItem item={item} />
